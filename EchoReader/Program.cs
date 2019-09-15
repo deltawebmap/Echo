f26f685a2934bfb193673ad41627e80d83a9d418
Roman-Port <darkwings220@gmail.com>
2019-09-15 14:15:32 -0500

    Add project files.

---
+++ EchoReader/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace EchoReader
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
